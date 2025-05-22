4b549dea84620f79ee9cec2ebab0e7261979e808
kaushalkalas-awesome <kaushalkalas8825@gmail.com>
2025-05-22 20:56:29 +0530

    functionality of to-do-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'to-do-list';
+  task="";
+  taskList:{id:number, name:string}[] = []
+  completeList:{name:string}[] = []
+
+  addtask(){
+    this.taskList.push({id:this.taskList.length+1,name:this.task})
+    this.task=""
+  }
+
+  completetask(num:number){
+    for(let completetask of this.taskList)
+      this.completeList.push({name:completetask.name})
+    this.taskList.splice(num,1)
+  }
+
+  deletetask(num:number){
+    this.taskList.splice(num,1)
+    console.log(this.taskList)
+  }
 }
